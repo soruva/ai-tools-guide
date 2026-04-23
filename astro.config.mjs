52699a4a81999760d4e4a87ac6106a31ca58ccdb
SoruvaLab <soruvalab@gmail.com>
2026-04-23 14:25:46 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,14 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://ai-tools-guide-345.pages.dev',
+
+  markdown: {
+    shikiConfig: {
+      theme: 'github-dark'
+    }
+  },
+
+  build: {
+    format: 'directory'
+  }
 });
